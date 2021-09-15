9dbcc24ddaaa5e3cd70044e717ec7239b3790aba
Jake Park <qkstjr09@gmail.com>
2021-09-15 17:31:45 +0900

    Feat: Set ESLint Airbnb

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};

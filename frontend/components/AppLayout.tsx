5778d2b4520788c37fccd2296729fd5c449218d2
Jake Park <qkstjr09@gmail.com>
2021-09-15 19:07:28 +0900

    Feat: Create Layout Component

--- frontend/components/AppLayout.tsx
+++ frontend/components/AppLayout.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
+import { Menu } from 'antd';
 
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
-export default function AppLayout( { children }: AppLayoutProps) {
+export default function AppLayout({ children }: AppLayoutProps): JSX.Element {
   return (
     <div>
-      <div>
-        <Link href="/"><a>노드버드</a></Link>
-        <Link href="/profile"><a>프로필</a></Link>
-        <Link href="/signup"><a>회원가입</a></Link>
-      </div>
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/">노드버드</Link>
+        </Menu.Item>
+        <Menu.Item>
+        <Link href="/profile">프로필</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup">회원가입</Link>
+        </Menu.Item>
+      </Menu>
       { children }
     </div>
   );

cbb73948e5d23930b9b6d644ac5c405ca7a54542
Jake Park <qkstjr09@gmail.com>
2021-09-16 17:53:01 +0900

    Feat: Seperate TopMenu

--- frontend/components/AppLayout.tsx
+++ frontend/components/AppLayout.tsx
@@ -1,13 +1,8 @@
 import React, { useState } from 'react';
-import Link from 'next/link';
-import { Menu, Input, Row, Col } from 'antd';
-import styled from '@emotion/styled';
+import { Row, Col } from 'antd';
 import LoginForm from './LoginForm';
 import UserProfile from './UserProfile';
-
-const SearchInput = styled(Input.Search)`
-  vertical-align: middle;
-`;
+import TopMenu from './TopMenu';
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -17,22 +12,7 @@ export default function AppLayout({ children }: AppLayoutProps): JSX.Element {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-      <Menu mode="horizontal">
-        <Menu.Item>
-          <Link href="/">노드버드</Link>
-        </Menu.Item>
-        <Menu.Item>
-          <Link href="/profile">프로필</Link>
-        </Menu.Item>
-        <Menu.Item>
-            <SearchInput
-              enterButton
-            />
-        </Menu.Item>
-        <Menu.Item>
-          <Link href="/signup">회원가입</Link>
-        </Menu.Item>
-      </Menu>
+      <TopMenu />
       <Row gutter={8}>
         <Col xs={24} md={6}>
           { isLoggedIn ? <UserProfile  setIsLoggedIn={setIsLoggedIn} /> : <LoginForm setIsLoggedIn={setIsLoggedIn} /> }

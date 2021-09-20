080b5d670bd598239a916c176058ee33c77904bb
Jake Park <qkstjr09@gmail.com>
2021-09-20 19:16:04 +0900

    Feat: Create Redux store script

---
+++ frontend/store/configureStore.ts
@@ -0,0 +1,22 @@
+import { createWrapper } from 'next-redux-wrapper';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from '../reducers';
+
+const configureStore = () => {
+  const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware())
+    : compose(composeWithDevTools(applyMiddleware()));
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const store = createStore(reducer, enhancer);
+
+  return store;
+};
+
+const wrapper = createWrapper(configureStore, {
+  debug: process.env.NODE_ENV === 'development',
+});
+
+export default wrapper;

77aaffce20ed86f7b67a5dc0f58e773c116e2142
Jake Park <qkstjr09@gmail.com>
2021-10-01 17:54:35 +0900

    Feat: Set Backend Dev URL

--- frontend/sagas/index.ts
+++ frontend/sagas/index.ts
@@ -1,7 +1,10 @@
 import { all, fork } from 'redux-saga/effects';
+import axios from 'axios';
 
 import userSaga from './user';
 import postSaga from './post';
+
+axios.defaults.baseURL = 'http://localhost:4000';
 
 export default function* rootSaga(): Generator {
   yield all([

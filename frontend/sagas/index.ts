05153e9d53310bd2ab3e75d6faf44148fa291953
Jake Park <qkstjr09@gmail.com>
2021-10-10 11:16:10 +0900

    Chore: change axiosRequest function
    modify get method code

--- frontend/sagas/index.ts
+++ frontend/sagas/index.ts
@@ -5,12 +5,13 @@ import userSaga from './user';
 import postSaga from './post';
 
 axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.withCredentials = true;
 
 export function axiosRequest(method: string, uri: string, data: any) {
   switch(method.toUpperCase()) {
-    case 'GET': return axios.get(uri, data);
+    case 'GET': return axios.get(uri);
     case 'POST': return axios.post(uri, data);
-    default: return axios.get(uri, data);
+    default: return axios.get(uri);
   }
 }
 
